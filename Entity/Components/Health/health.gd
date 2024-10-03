c0f89eb999652358e4b258d9d3bfd3232dc28f5e
mateustk <mateustk@stud.ntnu.no>
2024-10-03 15:02:27 +0200

    Weeks work in one commit

---
+++ Entity/Components/Health/health.gd
@@ -0,0 +1,50 @@
+class_name Health
+extends Node
+
+signal death
+signal health_changed(damage: int)
+
+var max_health: int = 100
+var invunerability: bool = false 
+
+@onready var health: int = max_health
+
+#Changes the value of invunerability
+func toggle_invulnerability():
+	invunerability = !invunerability
+
+#Returns current health
+func get_current_health() -> int:
+	return health
+
+#Returns max_health
+func get_max_health() -> int:
+	return max_health
+
+#Sets health to a new value
+func set_health(new_value: int):
+	health = new_value
+
+#Sets max_health to a new value
+func set_max_health(new_value: int):
+	max_health = new_value
+
+#Handles damage
+func take_damage(damage: int):
+	if invunerability == false:
+		health -= damage
+	health_changed.emit(damage)
+	
+	if health <= 0: #Health 0 or bellow means death
+		death.emit()
+
+#Healing that will not allow to go over max_health
+func heal_up_to_max_health(healing: int):
+	if healing + health > max_health:
+		health = max_health
+	else:
+		health += healing
+
+#Healing that will allow healing over max_health
+func heal_over_max_health(healing: int):
+	health += healing
