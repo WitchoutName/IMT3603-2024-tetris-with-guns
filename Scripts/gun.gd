85379ba452bb350e21e5fc92adcdf09277dc2df4
elias-ek <eliasek@stud.ntnu.no>
2024-10-03 12:57:52 +0200

    First basic shooting mechanics
    aiming, firing, bullet spread, case ejecting

---
+++ Scripts/gun.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+var bulletScene = load("res://Entities/bullet.tscn")
+var casingScene = load("res://Entities/casing.tscn")
+var bulletSpeed = 1000
+var bps = 2
+var bulletDamage = 30
+var bulletSpread = 0.1
+var fireRate: float
+var timeUntilFire = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	fireRate = 1 / bps
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("click") and timeUntilFire > fireRate:
+		var bullet = bulletScene.instantiate()
+		var casing = casingScene.instantiate()
+		if $AnimatedSprite2D.is_playing():
+			$AnimatedSprite2D.stop()
+		
+		owner.add_child(bullet)
+		bullet.rotation = $Barrel.global_rotation + randf_range(-bulletSpread,bulletSpread)
+		bullet.position = $Barrel.global_position
+		bullet.linear_velocity = bullet.transform.x * bulletSpeed
+		
+		
+		$AnimatedSprite2D.play("Fire")
+		owner.add_child(casing)
+		casing.rotation = $Eject.global_rotation + randf_range(-0.25, 0)
+		casing.position = $Eject.global_position
+		casing.linear_velocity = casing.transform.y * -150
+		timeUntilFire = 0
+		
+		
+	
+	else:
+		timeUntilFire += delta
+		
