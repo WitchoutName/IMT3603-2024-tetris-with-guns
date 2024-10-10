18527d8180e93b8e4d37466e6f90312ca762ca60
Ruben Singer <ruben.singer@stud.htwk-leipzig.de>
2024-10-10 15:33:48 +0200

    feat: Movement

---
+++ Entities/Player/STATES.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var JUMP = $JUMP
+@onready var Fall = $FALL
+@onready var Idle = $IDLE
+@onready var MOVE = $MOVE
+@onready var Dash = $DASH
+@onready var SlIDE = $SLIDE
+#@onready var CLIMB = $CLIMB
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	pass
