2270372c6d45ff3283f99f808d7ae0457cf6c054
Tomas Foltyn <folyn.tomas1@gmail.com>
2024-10-14 19:43:55 +0200

    Merge main for feat/tower

---
+++ Entities/Player/JUMP.gd
@@ -0,0 +1,15 @@
+extends "state.gd"
+
+func update(delta):
+	player_movement()
+	Player.gravity(delta)
+	if Player.velocity.y>0:
+		return STATES.FALL
+	if Player.dash_input and Player.can_dash:
+		return STATES.DASH
+	if Player.get_next_to_wall() !=0:
+		return STATES.SLIDE
+	return null
+
+func enter_state():
+	Player.velocity.y = Player.JUMP_VELOCITY
