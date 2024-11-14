c02da5efc0f1b3f79a7aa398fc83aad443f27664
elias-ek <eliasek@stud.ntnu.no>
2024-11-14 13:26:41 +0100

    made placeholder sprites for rifle and shotgun, bullets now despawn on impact, started on shotgun

--- Entities/Guns/bullet.gd
+++ Entities/Guns/bullet.gd
@@ -4,20 +4,15 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Timer.start()
-
+	contact_monitor = true
+	max_contacts_reported = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node) -> void:
-	
-	if is_instance_of(body, Player):
-		queue_free()
-	elif is_instance_of(body, Tetramino2):
-		queue_free()
-	else:
-		queue_free()
+	queue_free()
 		
 func _on_timeout():
 	queue_free()
